81a8c44063836c176d6d8a89ef7c30c6757b81ad
tiffanyyeh2024 <160838954+tiffanyyeh2024@users.noreply.github.com>
2024-03-28 15:33:00 +0800

    Update AddBooking.sql

--- AddBooking.sql
+++ AddBooking.sql
@@ -1,10 +1,8 @@
 USE little_lemon_db;
 DROP PROCEDURE IF EXISTS AddBooking;
 DELIMITER //
-
 CREATE PROCEDURE AddBooking(IN bookingid INT, IN customerid INT,  IN bookingdate DATE, IN tablenumber INT) BEGIN
-  INSERT INTO bookings (BookingID, BookingDate, TbleNumber, CustomerID) VALUES (bookingid, bookingdate, tablenumber, customerid);
+  INSERT INTO bookings(BookingID, BookingDate, TbleNumber, CustomerID) VALUES (bookingid, bookingdate, tablenumber, customerid);
 END//
-
 DELIMITER ;
-CALL AddBooking(1,2,'2024/03/22',3);
+CALL AddBooking(1,2,'2024/03/28',3);
