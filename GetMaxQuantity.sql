1e70604a8c85a15a0a1017f5461a42a781a2515e
tiffanyyeh2024 <160838954+tiffanyyeh2024@users.noreply.github.com>
2024-03-28 15:21:12 +0800

    Update GetMaxQuantity.sql

--- GetMaxQuantity.sql
+++ GetMaxQuantity.sql
@@ -1,3 +1,3 @@
-USE littlelemondb;
-CREATE PROCEDURE GetMaxQuantity() SELECT MAX(quantity) AS `Max Quantity in Order` FROM Orders;
+USE little_lemon_db;
+CREATE PROCEDURE GetMaxQuantity() SELECT MAX(Quantity) AS `Max Quantity in Order` FROM Orders;
 CALL GetMaxQuantity();
