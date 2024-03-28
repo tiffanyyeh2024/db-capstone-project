0c6907624cc455f8eda742a58eccacec43f87148
tiffanyyeh2024 <160838954+tiffanyyeh2024@users.noreply.github.com>
2024-03-28 15:19:54 +0800

    Update AddBooking.sql

--- AddBooking.sql
+++ AddBooking.sql
@@ -2,9 +2,9 @@ USE littlelemondb;
 DROP PROCEDURE IF EXISTS AddBooking;
 DELIMITER //
 
-CREATE PROCEDURE AddBooking(IN booking_id INT, IN customer_id INT,  IN booking_date DATE, IN table_number INT) BEGIN
-  INSERT INTO bookings (booking_id, date, table_number, customer_id) VALUES (booking_id, booking_date, table_number, customer_id);
-END
+CREATE PROCEDURE AddBooking(IN bookingid INT, IN customerid INT,  IN bookingdate DATE, IN tablenumber INT) BEGIN
+  INSERT INTO bookings (BookingID, BookingDate, TbleNumber, CustomerID) VALUES (bookingid, bookingdate, tablenumber, customerid);
+END//
 
 DELIMITER ;
 CALL AddBooking(1,2,'2024/03/22',3);
